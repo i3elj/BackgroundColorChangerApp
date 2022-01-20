c0a05ec8a8d27b5146cbf21d0091f736565c3ebf
i3elj <nardoguisande@gmail.com>
2022-01-20 15:48:48 -0300

    few changes into design

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,13 @@ import '../style/App.css';
 import Button from '../components/Button'
 import InputColor from '../components/InputColor'
 
-const App = () => {
-    return (
-        <div className="App">
-            <header id="App-header">
-                <InputColor />
-                <Button />
-            </header>
-        </div>
-    );
-}
+const App = () => {return(
+    <div className="App">
+        <header id="App-header">
+            <InputColor />
+            <Button />
+        </header>
+    </div>
+)}
 
 export default App;
--- src/controllers/colors.js
+++ src/controllers/colors.js
@@ -1,3 +1,5 @@
+// This function get the value inside a input field and uses it
+// to change the background of the page
 function changeBackgroundColor() {
     let inputValue = document.getElementById("input").value;
     document.getElementById("App-header").style.backgroundColor = inputValue;
