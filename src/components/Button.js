00dccdc58619bfcfa72b0d861376deafccfd4d1b
i3elj <nardoguisande@gmail.com>
2022-01-22 13:01:29 -0300

    adjusts

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,4 @@
 import "../style/Button.css";
-import InputColor from "../components/InputColor";
-import App from "../components/App";
 import { changeBackgroundColor } from "../controllers/colors";
 
 const Button = () => {
